51b07c0007de7dc6847ef0e2d6c51a96c5187c24
CodingPoka <kingshukbd.01@gmail.com>
2026-01-12 03:29:00 +0600

    final

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const paymentRoutes = require("./api/routes/paymentRoutes");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/payment", paymentRoutes);
+
+app.get("/", (req, res) => {
+  res.send("EduTech backend is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
